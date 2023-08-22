02af7fb3b39159ac474911a2e5fed1b594461932
Kabir <kabirvirk51@gmail.com>
2023-08-21 20:55:37 -0400

    Took a break, came back and forgot to keep commiting. However, here is the final product

--- script.js
+++ script.js
@@ -1,40 +1,120 @@
-const add = function (arg1, arg2) {
-  return arg1 + arg2;
-};
+let firstOperand = "";
+let currentDisplay = "0";
+let currentOperator = null;
+let shouldResetScreen = false;
 
-const subtract = function (arg1, arg2) {
-  return arg1 - arg2;
-};
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById("equalsBtn");
+const clearButton = document.getElementById("clearBtn");
+const deleteButton = document.getElementById("deleteBtn");
+const decButton = document.getElementById("decBtn");
+const lastDisplayScreen = document.getElementById("lastDisplayScreen");
+const currentDisplayScreen = document.getElementById("currentDisplayScreen");
 
-const multiply = function (arg1, arg2) {
-  return arg1 * arg2;
-};
+equalsButton.addEventListener("click", evaluate);
+clearButton.addEventListener("click", clear);
+deleteButton.addEventListener("click", deleteNumber);
+decButton.addEventListener("click", appendPoint);
 
-const divide = function (arg1, arg2) {
-  if (arg1 == 0 && arg2 == 0) {
-    return 0;
-  } else if (arg2 === 0) {
-    return "Error";
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => setOperator(button.textContent))
+);
+
+function appendNumber(number) {
+  if (currentDisplay === "0" || shouldResetScreen) {
+    resetScreen();
   }
-  return arg1 / arg2;
-};
+  currentDisplay += number;
+  updateDisplay();
+}
 
-let firstNum;
-let operator;
-let secNum;
+function resetScreen() {
+  currentDisplay = "";
+  shouldResetScreen = false;
+}
 
-const operate = function (operator, firstNum, secNum) {
-  operator.toLowerCase();
+function clear() {
+  currentDisplay = "0";
+  firstOperand = "";
+  currentOperator = null;
+  updateDisplay();
+}
 
-  if (operator == "add") {
-    return add(firstNum, secNum);
-  } else if (operator == "subtract") {
-    return subtract(firstNum, secNum);
-  } else if (operator == "multiply") {
-    return multiply(firstNum, secNum);
-  } else if (operator == "divide") {
-    return divide(firstNum, secNum);
-  } else {
-    return "Invalid Operator";
+function appendPoint() {
+  if (shouldResetScreen) {
+    resetScreen();
   }
-};
+  if (currentDisplay === "") {
+    currentDisplay = "0";
+  }
+  if (currentDisplay.includes(".")) {
+    return;
+  }
+  currentDisplay += ".";
+  updateDisplay();
+}
+
+function deleteNumber() {
+  currentDisplay = currentDisplay.slice(0, -1);
+  updateDisplay();
+}
+
+function setOperator(operator) {
+  if (currentOperator !== null) {
+    evaluate();
+  }
+  firstOperand = currentDisplay;
+  currentOperator = operator;
+  lastDisplayScreen.textContent = `${firstOperand} ${currentOperator}`;
+  resetScreen();
+}
+
+function evaluate() {
+  if (currentOperator === null || shouldResetScreen) {
+    return;
+  }
+  if (currentOperator === "÷" && currentDisplay === "0") {
+    alert("You can't divide by 0!");
+    clear();
+    return;
+  }
+  const secondOperand = currentDisplay;
+  const result = operate(
+    currentOperator,
+    parseFloat(firstOperand),
+    parseFloat(secondOperand)
+  );
+  currentDisplay = result.toString();
+  updateDisplay();
+  firstOperand = currentDisplay;
+  currentOperator = null;
+  lastDisplayScreen.textContent = "";
+}
+
+function updateDisplay() {
+  currentDisplayScreen.textContent = currentDisplay;
+}
+
+function operate(operator, firstNum, secNum) {
+  switch (operator) {
+    case "+":
+      return firstNum + secNum;
+    case "−":
+      return firstNum - secNum;
+    case "×":
+      return firstNum * secNum;
+    case "÷":
+      if (secNum === 0) {
+        return "Error";
+      } else {
+        return firstNum / secNum;
+      }
+    default:
+      return "Error";
+  }
+}
